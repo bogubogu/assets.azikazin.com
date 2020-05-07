1bfebaaf8cd9a50ae91f98ef8333faef0c100b3f
bogubogu <63712967+bogubogu@users.noreply.github.com>
2020-05-07 21:23:42 +0900

    Update url.js

--- url.js
+++ url.js
@@ -1,4 +1,6 @@
-if ( window.location.pathname == '/tagged/plastics' ) {
-	var root = document.documentElement;
-	root.className += 'tagpage';
-}
+if ( window.location.pathname == '/' ) { document.write( '<style>#landingpage{display:block;}</style>' ); } 
+    if ( window.location.pathname == '/tagged/noodlehead' ) { document.write( '<style>#noodlehead{display:block;}</style>' ); } 
+    if ( window.location.pathname == '/tagged/chopnbeat' ) { document.write( '<style>#chopnbeat{display:block;}</style>' ); } 
+    if ( window.location.pathname == '/tagged/plastics' ) { document.write( '<style>#plastics{display:block;}</style>' ); } 
+        if ( window.location.pathname == '/tagged/sfbd' ) { document.write( '<style>#sfbd{display:block;}</style>' ); } 
+            if ( window.location.pathname == '/tagged/bts' ) { document.write( '<style>#bts{display:block;}</style>' ); } 
