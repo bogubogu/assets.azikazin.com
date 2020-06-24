55178ff5653c7b65f484d20a330478b5eb237bbb
bogubogu <63712967+bogubogu@users.noreply.github.com>
2020-06-24 15:06:11 +0900

    Delete url.js

--- url.js
+++
@@ -1,6 +0,0 @@
-if ( window.location.pathname == '/' ) { document.write( '<style>#landingpage{display:block;}</style>' ); } 
-    if ( window.location.pathname == '/tagged/noodlehead' ) { document.write( '<style>#noodlehead{display:block;}</style>' ); } 
-    if ( window.location.pathname == '/tagged/chopnbeat' ) { document.write( '<style>#chopnbeat{display:block;}</style>' ); } 
-    if ( window.location.pathname == '/tagged/plastics' ) { document.write( '<style>#plastics{display:block;}</style>' ); } 
-        if ( window.location.pathname == '/tagged/sfbd' ) { document.write( '<style>#sfbd{display:block;}</style>' ); } 
-            if ( window.location.pathname == '/tagged/bts' ) { document.write( '<style>#bts{display:block;}</style>' ); } 
